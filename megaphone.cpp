1f64c7da6d82bbe305966bf11046011c5ced698a
Alvaro Jurado <quickalvaro@gmail.com>
2024-04-16 23:28:37 +0200

    ex00 process

---
+++ megaphone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc != 1)
+	{
+		for (int i = 0; i < argc, i++)
+		{
+			std::string args(argv[i]);
+		}
+	}
+	return (0);
+}
